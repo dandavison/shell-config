576547916ee02fa5368b0bbe21a5ec3ba5e84107
Dan Davison <dandavison7@gmail.com>
2011-09-01 23:16:55 -0700

    Budget completion for gd, gs, gl aliases

--- completion.sh
+++ completion.sh
@@ -3,3 +3,6 @@
 export FIGNORE=.pyc:#:.DS_Store:.git
 source ~/lib/bash/bash_completion
 source ~/lib/bash/git-completion.bash
+
+complete -W "--stat" gd gs
+complete -W "--oneline --grep --author" gl
