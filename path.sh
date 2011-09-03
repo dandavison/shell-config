f50ca9fc5b0a446041ee656cb4b12d91a2cdf2cf
Dan Davison <dandavison7@gmail.com>
2011-09-02 23:48:53 -0700

    /usr/local -> /usr/homebrew in paths
    Makes it more explicit that /usr/local is all homebrew stuff (it's a
    symlink).

--- path.sh
+++ path.sh
@@ -2,23 +2,24 @@
 
 if [ "$_dan_system" = "Darwin" ] ; then
     for path in $(cat <<EOF
-/usr/local/bin
-/usr/local/share/python
-/usr/local/Cellar/python/2.7.2/bin
+/usr/homebrew/bin
+/usr/homebrew/share/python
+/usr/homebrew/Cellar/python/2.7.2/bin
+/usr/homebrew/share/npm/bin
+/usr/homebrew/Cellar/ruby/1.9.2-p290/bin
 /usr/texbin
-/usr/local/share/npm/bin
 EOF
     ) ; do
 	export PATH=$path:$PATH
     done
 
-    export PYTHON_SITE_PACKAGES=/usr/local/Cellar/python/2.7.2/lib/python2.7/site-packages
+    export PYTHON_SITE_PACKAGES=/usr/homebrew/Cellar/python/2.7.2/lib/python2.7/site-packages
 else
-    export PATH=/usr/local/src/postgresql/bin:$PATH
+    export PATH=/usr/homebrew/src/postgresql/bin:$PATH
 fi
 
 export PATH=$HOME/bin:$PATH
-export NODE_PATH=/usr/local/lib/node
+export NODE_PATH=/usr/homebrew/lib/node
 
 export PATH=$(_dan_uniquify_path $PATH)
 export PYTHONPATH=$(_dan_uniquify_path $PYTHONPATH)
