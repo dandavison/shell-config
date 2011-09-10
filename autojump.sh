5c9c3ae943783a11e855028698b2ff82dd9751ae
Dan Davison <dandavison7@gmail.com>
2011-09-10 11:40:49 -0700

    Generic location for autojump library

--- autojump.sh
+++ autojump.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 
-if [ -f `brew --prefix`/etc/autojump ]; then
-  . `brew --prefix`/etc/autojump
-  # Redefine autojump's j so as not to echo the new location
-  function j { new_path="$(autojump $@)"; [ -n "$new_path" ] && cd "$new_path" ; }
-fi
+source ~/lib/bash/autojump
+
+# Redefine autojump's j so as not to echo the new location
+function j { new_path="$(autojump $@)"; [ -n "$new_path" ] && cd "$new_path" ; }
