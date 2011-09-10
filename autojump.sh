1c5183f6c0cc730e960b5f2a53747a789748ff28
Dan Davison <dandavison7@gmail.com>
2011-09-10 11:45:56 -0700

    Fix generic autojump config

--- autojump.sh
+++ autojump.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 
-source ~/lib/bash/autojump
 
-# Redefine autojump's j so as not to echo the new location
-function j { new_path="$(autojump $@)"; [ -n "$new_path" ] && cd "$new_path" ; }
+# If conditions copied from /usr/homebrew/etc/autojump
+if [ "$BASH_VERSION" ] && [ -n "$PS1" ] && echo $SHELLOPTS | grep -v posix >>/dev/null; then
+
+    . ~/lib/bash/autojump.bash
+    # Redefine autojump's j so as not to echo the new location
+    function j { new_path="$(autojump $@)"; [ -n "$new_path" ] && cd "$new_path" ; }
+fi
