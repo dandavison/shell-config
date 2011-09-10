77f6f01d4c1b1b228fe01ba4860c2b77a3e83a93
Dan Davison <dandavison7@gmail.com>
2011-09-10 11:34:26 -0700

    Set bash completion environment variables

--- completion.sh
+++ completion.sh
@@ -1,7 +1,12 @@
 #!/usr/bin/env bash
 
 export FIGNORE=.pyc:#:.DS_Store:.git
+
+BASH_COMPLETION=~/lib/bash/bash_completion
+BASH_COMPLETION_DIR=~/lib/bash/bash_completion.d
+BASH_COMPLETION_COMPAT_DIR=~/lib/bash/bash_completion.d
 source ~/lib/bash/bash_completion
+
 source ~/lib/bash/git-completion.bash
 
 complete -W "--stat --word-diff" gd gs
