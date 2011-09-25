1cb1581d5f938f05d8c7e4a2416a31b60fcd89d9
Dan Davison <dandavison7@gmail.com>
2011-09-25 00:43:41 -0700

    Tidy up shell-specific prompt config

--- prompt.sh
+++ prompt.sh
@@ -1,18 +1,3 @@
-black=30 ; red=31 ; green=32 ; yellow=33 ; blue=34 ; magenta=35 ; cyan=36 ; white=37
-
-if [ `whoami` = 'root' ] ; then
-    prompt_char='#'
-    prompt_col=$red
-else
-    prompt_char=''
-    prompt_col=$cyan
-fi
-
-_host=$HOSTNAME:
-if [ $_dan_system = 'Darwin' ] ; then
-    _host=''
-fi
-
 GIT_PS1_SHOWDIRTYSTATE=yes
 GIT_PS1_UNSTAGED="અ "
 GIT_PS1_STAGED="જ "
@@ -25,6 +10,21 @@ if [[ -n ${ZSH_VERSION-} ]]; then
     PROMPT+="%{$fg[red]%}%(?..(%?%))%{$reset_color%} " # exit status if nonzero
     RPROMPT=$'%{${fg[green]}%}$(__git_ps1)%{${fg[default]}%}'
 else
+    black=30 ; red=31 ; green=32 ; yellow=33 ; blue=34 ; magenta=35 ; cyan=36 ; white=37
+    
+    if [ `whoami` = 'root' ] ; then
+        prompt_char='#'
+        prompt_col=$red
+    else
+        prompt_char=''
+        prompt_col=$cyan
+    fi
+    
+    _host=$HOSTNAME:
+    if [ $_dan_system = 'Darwin' ] ; then
+        _host=''
+    fi
+    
     export PROMPT_COMMAND='PS1="$(_colorize $prompt_col $_host$(_dan_abbreviate_path $(pwd)))$(_colorize $red "$(__git_ps1 "(%s)")") "'
     PS2=''
 fi
