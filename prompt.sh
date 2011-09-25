eac7abc8c69b996a088afc1d31987abc2053add5
Dan Davison <dandavison7@gmail.com>
2011-09-24 22:56:55 -0700

    Shell-specific prompt config

--- prompt.sh
+++ prompt.sh
@@ -16,6 +16,15 @@ fi
 GIT_PS1_SHOWDIRTYSTATE=yes
 GIT_PS1_UNSTAGED="અ "
 GIT_PS1_STAGED="જ "
-export PROMPT_COMMAND='PS1="$(_colorize $prompt_col $_host$(_dan_abbreviate_path $(pwd)))$(_colorize $red "$(__git_ps1 "(%s)")") "'
 
-PS2=''
+if [[ -n ${ZSH_VERSION-} ]]; then
+    setopt PROMPT_SUBST
+    # PROMPT="%{$fg[cyan]%}%n%{$reset_color%}@" # name@
+    # PROMPT+="%{$fg[cyan]%}%m%{$reset_color%}" # hostname
+    PROMPT="%{$fg[cyan]%}%20<..<%~%{$reset_color%}" # cwd (truncated to 20 chars)
+    PROMPT+="%{$fg[red]%}%(?..(%?%))%{$reset_color%} " # exit status if nonzero
+    RPROMPT=$'%{${fg[green]}%}$(__git_ps1)%{${fg[default]}%}'
+else
+    export PROMPT_COMMAND='PS1="$(_colorize $prompt_col $_host$(_dan_abbreviate_path $(pwd)))$(_colorize $red "$(__git_ps1 "(%s)")") "'
+    PS2=''
+fi
--- zshrc
+++ zshrc
@@ -4,10 +4,13 @@ cd ~/config/shell
 autoload -U compinit
 compinit
 
+autoload -U colors
+colors
+
 source lib.sh
 source env.sh
 source zsh/path.sh
-# source prompt.sh
+source prompt.sh
 # source history.sh
 source ~/lib/git/contrib/completion/git-completion.bash
 # source autojump.sh
