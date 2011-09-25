3d9da94ff8921518737bf24f79f5d7a2c1e065ea
Dan Davison <dandavison7@gmail.com>
2011-09-25 00:43:08 -0700

    Misc zsh config

---
+++ zsh/zsh.sh
@@ -0,0 +1,33 @@
+autoload -U compinit
+compinit
+
+autoload -U colors
+colors
+
+zstyle ':completion:*' use-cache on
+zstyle ':completion:*' cache-path ~/.zsh/cache
+zstyle ':completion:*:functions' ignored-patterns '_*' # ignore for absent commands
+
+setopt extended_glob
+setopt AUTO_CD
+setopt CHASE_LINKS
+
+setopt SHARE_HISTORY
+setopt HIST_IGNORE_DUPS
+setopt HIST_FIND_NO_DUPS
+setopt HIST_EXPIRE_DUPS_FIRST
+setopt HIST_NO_STORE
+setopt HIST_REDUCE_BLANKS
+
+HISTSIZE=999999999999999999
+SAVEHIST=999999999999999999
+HISTFILE=~/zsh_.history
+
+unsetopt CLOBBER
+unsetopt BEEP
+
+setopt NOHUP
+setopt NO_BG_NICE
+
+# set INFOPATH for emacs info file reading
+
