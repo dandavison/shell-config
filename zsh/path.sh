f0c412731c1ec87602fd637649f7928c2ecc5a9b
Dan Davison <dandavison7@gmail.com>
2011-09-24 19:11:00 -0700

    Shell-specific path config

---
+++ zsh/path.sh
@@ -0,0 +1,5 @@
+typeset -U path
+path=(~/bin /usr/homebrew/src/postgresql/bin /usr/homebrew/bin /usr/homebrew/share/python /usr/homebrew/Cellar/python/2.7.2/bin /usr/homebrew/share/npm/bin /usr/homebrew/Cellar/ruby/1.9.2-p290/bin /usr/texbin .)
+
+export PYTHON_SITE_PACKAGES=/usr/homebrew/Cellar/python/2.7.2/lib/python2.7/site-packages
+export NODE_PATH=/usr/homebrew/lib/node
