4fb2aa4c87f38f566b069e1156b3b8f645bfc2a1
Dan Davison <dandavison7@gmail.com>
2011-10-15 09:34:20 -0700

    Truncate prompt wd to last 2 directory components

--- prompt.sh
+++ prompt.sh
@@ -6,7 +6,7 @@ if [[ -n ${ZSH_VERSION-} ]]; then
     setopt PROMPT_SUBST
     # PROMPT="%{$fg[cyan]%}%n%{$reset_color%}@" # name@
     # PROMPT+="%{$fg[cyan]%}%m%{$reset_color%}" # hostname
-    PROMPT="%{$fg[cyan]%}%20<..<%~%{$reset_color%}" # cwd (truncated to 20 chars)
+    PROMPT="%{$fg[cyan]%}%2~%{$reset_color%}"
     PROMPT+="%{$fg[red]%}%(?..(%?%))%{$reset_color%} " # exit status if nonzero
     RPROMPT=$'%{${fg[green]}%}$(__git_ps1)%{${fg[default]}%}'
 else
