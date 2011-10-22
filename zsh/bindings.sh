18169260307da9ae65ccf34298bfb917464587f2
Dan Davison <dandavison7@gmail.com>
2011-10-21 21:41:39 -0700

    zsh key bindings

---
+++ zsh/bindings.sh
@@ -0,0 +1,5 @@
+bindkey '\ep' history-beginning-search-backward
+bindkey '\en' history-beginning-search-forward
+
+bindkey '\e[A' history-beginning-search-backward  # up
+bindkey '\e[B' history-beginning-search-forward   # down
