e99a4aa2553471bb66d4505f3c25b19a55c20618
Dan Davison <dandavison7@gmail.com>
2011-10-23 08:43:04 -0700

    M-up and M-down send different sequences under tmux

--- zsh/bindings.sh
+++ zsh/bindings.sh
@@ -4,7 +4,14 @@ bindkey '\en' history-beginning-search-forward
 # bindkey '\e[A' history-beginning-search-backward  # up
 # bindkey '\e[B' history-beginning-search-forward   # down
 
+# On the macbook, M-up/down send these sequences normally
 bindkey '\e[1;9A' history-beginning-search-backward   # M-up
 bindkey '\e[1;9B' history-beginning-search-forward   # M-down
+
+# but these under tmux
+bindkey '\e^[[A' history-beginning-search-backward  # M-up
+bindkey '\e^[[B' history-beginning-search-forward   # M-down
+
+
 
 bindkey '^w' kill-region
