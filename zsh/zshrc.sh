63977ab7a821740c21942eed9695c7a6d9de0b02
Dan Davison <dandavison7@gmail.com>
2011-10-22 22:11:06 -0700

    rename zshrc -> zshrc.sh

--- zsh/zshrc.sh
+++ zsh/zshrc.sh
@@ -1,10 +1,12 @@
 _cwd=$(pwd)
 cd ~/config/shell
 
+# [[ $TERM != screen ]] && tmux
+
 source lib.sh
 source git-functions.sh
 source env.sh
-source zsh/zsh.sh
+source zsh/zsh_misc.sh
 source zsh/path.sh
 source prompt.sh
 source autojump.sh
