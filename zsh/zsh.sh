de38c22ee523af99ef210fc94b030af35af2dfa0
Dan Davison <dandavison7@gmail.com>
2011-10-31 22:51:11 -0700

    Rearrage misc zsh config

--- zsh/zsh.sh
+++ zsh/zsh.sh
@@ -1,3 +1,21 @@
+setopt CLOBBER
+setopt rmstarsilent
+setopt AUTO_CD
+setopt CHASE_LINKS
+unsetopt BEEP
+setopt NOHUP
+setopt NO_BG_NICE
+
+setopt SHARE_HISTORY
+setopt HIST_IGNORE_DUPS
+setopt HIST_FIND_NO_DUPS
+setopt HIST_EXPIRE_DUPS_FIRST
+setopt HIST_NO_STORE
+setopt HIST_REDUCE_BLANKS
+HISTSIZE=999999999999999999
+SAVEHIST=999999999999999999
+HISTFILE=~/.zsh_history
+
 autoload -U colors
 colors
 
@@ -11,7 +29,7 @@ zstyle ':completion:*' cache-path ~/.zsh/cache
 zstyle ':completion:*:functions' ignored-patterns '_*' # ignore for absent commands
 zstyle ':completion:*' list-colors ${(s.:.)LS_COLORS}
 zstyle ':completion:*' verbose true
-zstyle ':completion:*' menu select=6
+zstyle ':completion:*' menu select=2
 
 # B2Z ch.14
 setopt complete_in_word
@@ -24,32 +42,17 @@ __git_files(){ _main_complete _files }
 
 # bindkey -M menuselect '^o' accept-and-infer-next-history
 
-setopt CLOBBER
-setopt rmstarsilent
-
-setopt AUTO_CD
-setopt CHASE_LINKS
-
-setopt SHARE_HISTORY
-setopt HIST_IGNORE_DUPS
-setopt HIST_FIND_NO_DUPS
-setopt HIST_EXPIRE_DUPS_FIRST
-setopt HIST_NO_STORE
-setopt HIST_REDUCE_BLANKS
-
-HISTSIZE=999999999999999999
-SAVEHIST=999999999999999999
-HISTFILE=~/.zsh_history
-
 fpath=(~/config/shell/zsh/functions $fpath)
 
 autoload edit-command-line-in-shell-mode
 zle -N edit-command-line-in-shell-mode
 bindkey '\ee' edit-command-line-in-shell-mode
 
-unsetopt BEEP
+unfunction _tmux
+autoload _tmux
 
-setopt NOHUP
-setopt NO_BG_NICE
+unfunction _tmux
+autoload _tmux
 
-set INFOPATH for emacs info file reading
+
+
