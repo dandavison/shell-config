641a87487185d8c4f850baaf3549c3465089518d
Dan Davison <dandavison7@gmail.com>
2011-11-02 09:26:40 -0700

    Source dircolors earlier in shell startup sequence
    $LS_COLORS is used to set colors for zsh completion

--- zsh/zshrc.sh
+++ zsh/zshrc.sh
@@ -18,11 +18,11 @@ cd ~/config/shell
 source lib.sh
 source git-functions.sh
 source env.sh
+source dircolors.sh
 source zsh/zsh.sh
 source path.sh
 source prompt.sh
 source autojump.sh
-source dircolors.sh
 source alias.sh
 source zsh/bindings.sh
 [[ -f extra.sh ]] && source extra.sh
