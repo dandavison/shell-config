101efb17b8264cf1419634f40a32ef47e8eed97b
Dan Davison <dandavison7@gmail.com>
2011-11-25 10:26:35 -0800

    Make autojump work on non-homebrew machines

--- autojump.sh
+++ autojump.sh
@@ -1,4 +1,9 @@
-__autojump_src=/usr/homebrew/etc/autojump
+__autojump_src_dir=~/lib/shell/autojump
+if [ "$BASH_VERSION" ] && [ -n "$PS1" ] && echo $SHELLOPTS | grep -v posix >>/dev/null; then
+        __autojump_src=$__autojump_src_dir/autojump.bash
+elif [ "$ZSH_VERSION" ] && [ -n "$PS1" ]; then
+        __autojump_src=$__autojump_src_dir/autojump.zsh
+fi
 
 if [ -e "$__autojump_src" ] ; then
     . $__autojump_src
