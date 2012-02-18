ff478be7b88280c4ccc65b3ceebdab3a8bbc4f46
Dan Davison <dandavison7@gmail.com>
2012-02-18 15:09:07 -0800

    fix path to node module executables

--- path.sh
+++ path.sh
@@ -6,7 +6,7 @@ if _dan_is_laptop ; then
 /usr/local/sbin
 /usr/texbin
 /usr/local/Cellar/emacs/HEAD/bin
-/usr/local/share/npm/bin
+$HOME/node_modules/coffee-script/bin
 /usr/local/Cellar/ruby/1.9.2-p290/bin
 /usr/local/Cellar/python/2.7.2/bin
 /usr/local/share/python
