a240d4a1c7185c2443eb488e130a6f04a49be5b4
Dan Davison <dandavison7@gmail.com>
2012-07-29 13:16:10 -0700

    CLOJURESCRIPT_HOME=/Users/Shared/lib/clojure/clojurescript

--- env.sh
+++ env.sh
@@ -2,3 +2,4 @@ export BROWSER=google-chrome
 export EDITOR='emacsclient > /dev/null'
 export INFOPATH=/usr/homebrew/share/info
 export PYTHONDONTWRITEBYTECODE=1
+export CLOJURESCRIPT_HOME=/Users/Shared/lib/clojure/clojurescript
