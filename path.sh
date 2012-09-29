acc9699d95353588a86a7a2dab9ee5e37dc5e83d
Dan Davison <dan@counsyl.com>
2012-09-29 14:54:47 -0700

    Simplify path settings

--- path.sh
+++ path.sh
@@ -1,16 +1,7 @@
 if _dan_is_laptop ; then
     for dir in $(cat <<EOF
-/bin
-/usr/bin
-/usr/homebrew/bin
-/usr/homebrew/sbin
-/usr/texbin
-/usr/homebrew/Cellar/emacs/HEAD/bin
-$HOME/node_modules/coffee-script/bin
-/usr/homebrew/Cellar/ruby/1.9.2-p290/bin
-/usr/homebrew/Cellar/python/2.7.2/bin
-/usr/homebrew/share/python
-$HOME/.cabal/bin
+/usr/local/bin
+/usr/local/share/python
 $HOME/bin
 EOF
     ) ; do
@@ -18,7 +9,5 @@ EOF
 	    echo "Directory doesn't exist: $dir" 1>&2
 	}
     done
-    export NODE_PATH=/usr/homebrew/lib/node
-    export PYTHONPATH=~/lib/python/ipython
-    export PERL5LIB=/usr/homebrew/lib/perl5/site_perl:${PERL5LIB}
+    export PATH="$(brew --prefix coreutils)/libexec/gnubin:$PATH"
 fi
