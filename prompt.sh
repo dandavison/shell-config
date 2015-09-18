06b8b33111e03e3c7295b5f677d9ad4e7984748a
Dan Davison <dan@counsyl.com>
2015-09-17 22:39:36 -0700

    Fix prompt

--- prompt.sh
+++ prompt.sh
@@ -1,9 +1,8 @@
-BLACK=30 ; RED=31 ; GREEN=32 ; YELLOW=33 ; BLUE=34 ; MAGENTA=35 ; CYAN=36 ; WHITE=37
-GIT_PS1_SHOWDIRTYSTATE=yes
-GIT_PS1_UNSTAGED="અ "
-GIT_PS1_STAGED="જ "
-__PROMPT_COL=$cyan
-
+export BLACK=30  RED=31  GREEN=32  YELLOW=33  BLUE=34  MAGENTA=35  CYAN=36  WHITE=37
+export __PROMPT_COL=$CYAN
+export GIT_PS1_SHOWDIRTYSTATE=yes
+export GIT_PS1_UNSTAGED="અ "
+export GIT_PS1_STAGED="જ "
 
 __prompt_command () {
     PS1=""
@@ -12,7 +11,7 @@ __prompt_command () {
     # Current directory
     PS1+="$(_colorize $__PROMPT_COL "$(pwd | sed "s,$HOME,~,")")"
     # Git repository
-    PS1+="$(_colorize $red "$(__git_ps1 "(%s)")")"
+    PS1+="$(_colorize $RED "$(__git_ps1 "(%s)")")"
     PS1+=" "
 }
 
