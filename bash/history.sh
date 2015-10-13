9a9fad6639b6951ba3e4435493d9aadf8b18b02b
Dan Davison <dan@counsyl.com>
2015-10-12 21:22:33 -0700

    Allow commands starting with "t" in history!!

--- bash/history.sh
+++ bash/history.sh
@@ -1,5 +1,3 @@
-# http://www.debian-administration.org/articles/543
-export HISTIGNORE="[ \t]*:&" # '&' supresses duplicate entries
 export HISTFILESIZE=10000
 export HISTTIMEFORMAT="%s "
 shopt -s cmdhist                             # store multiline entries as a single history entry
