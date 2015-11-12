bca68ed26f65e0cd5ba67dfe3b24998625c111b1
Dan Davison <dan@counsyl.com>
2015-11-11 23:33:37 -0800

    Display counts of containers in different states

--- prompt.sh
+++ prompt.sh
@@ -1,3 +1,5 @@
+# -*-coding: utf-8;-*-
+
 export __BLACK=30 __RED=31 __GREEN=32 __YELLOW=33 __BLUE=34 __MAGENTA=35 __CYAN=36 __WHITE=37
 export GIT_PS1_SHOWDIRTYSTATE=yes
 export GIT_PS1_UNSTAGED="અ "
@@ -15,13 +17,32 @@ __prompt_command () {
     PS1+=" "
 }
 
+
 __docker_compose_ps1 () {
+    [[ -n "$USE_DOCKER_COMPOSE_PS1" ]] || return
     local dc_ps1=""
     local containers=$(docker-compose ps -q 2>/dev/null)
     [[ -n "$containers" ]] || return
-    local n_containers=$(echo -n "$containers" | grep -c '^')
-    dc_ps1=" ($(printf "⚡%.0s " $(seq 1 $n_containers)))"
-    echo -n $(_colorize $__GREEN "$dc_ps1")
+    local counts=$(docker inspect --format "{{ .State.Status }}" $containers | sort | uniq -c)
+    dc_ps1=""
+    while read count state ; do
+        case $state in
+            running)
+                symbol="🍏"  # 'GREEN APPLE'
+                ;;
+            exited)
+                symbol="🍎"  # 'RED APPLE'
+                ;;
+            *)
+                echo "Invalid state: '$state'" 1>&2
+                return
+                ;;
+        esac
+        dc_ps1+="$(printf "${symbol}%.0s " $(seq 1 $count))"
+    done <<< "$counts"
+    dc_ps1=" ($dc_ps1)"
+    dc_ps1=$(_colorize $__CYAN "$dc_ps1")
+    echo -n "$dc_ps1"
 }
 
 export PROMPT_COMMAND=__prompt_command
