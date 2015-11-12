603b2ab653ef6895ce0141c9bcd65e852e413c8d
Dan Davison <dan@counsyl.com>
2015-11-11 22:26:46 -0800

    docker-compose prompt

--- prompt.sh
+++ prompt.sh
@@ -11,7 +11,17 @@ __prompt_command () {
     PS1+="$(_colorize $__CYAN "$(pwd | sed "s,$HOME,~,")")"
     # Git repository
     PS1+="$(_colorize $__RED "$(__git_ps1 "(%s)")")"
+    PS1+="$(__docker_compose_ps1)"
     PS1+=" "
+}
+
+__docker_compose_ps1 () {
+    local dc_ps1=""
+    local containers=$(docker-compose ps -q 2>/dev/null)
+    [[ -n "$containers" ]] || return
+    local n_containers=$(echo -n "$containers" | grep -c '^')
+    dc_ps1=" ($n_containers)"
+    echo -n $(_colorize $__GREEN "$dc_ps1")
 }
 
 export PROMPT_COMMAND=__prompt_command
