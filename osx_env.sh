8148d52da6eb8f0edae874d330c9d48a4021a05e
Dan Davison <dandavison7@gmail.com>
2016-01-02 18:07:11 +0000

    Don't set github API token

--- osx_env.sh
+++ osx_env.sh
@@ -14,4 +14,3 @@ source $(which virtualenvwrapper.sh)
 
 PSQL_EDITOR="emacsclient --eval \"(setq-default major-mode 'sql-mode)\"; emacsclient"
 
-export HOMEBREW_GITHUB_API_TOKEN=cf4f93adb741e12d0b5ceaeca3f4235615a336a2
