ca7ca66bba4b98b2d2c5d293f329f9ed11571e38
Dan Davison <dandavison7@gmail.com>
2016-04-24 11:17:38 -0700

    Revert "Automatically set all variables for export"
    This reverts commit 54438cd78c83b5bd401f14a0bdf43d89bc86133e.

--- bash/bashrc.sh
+++ bash/bashrc.sh
@@ -1,8 +1,6 @@
 _cwd=$(pwd)
 cd ~/src/1p/shell-config
 
-set -a
-
 . lib.sh
 . path.sh
 . git-functions.sh
@@ -19,8 +17,6 @@ __dan_is_osx && . osx_env.sh
 [ -f extra.sh ] && . extra.sh
 . bash/alias_completion.sh
 . tmux.sh
-
-set +a
 
 # confused
 export PROMPT_COMMAND=__prompt_command
