3a141611cf2bc1a3ed95fa376ba555b87ad9e617
Dan Davison <dandavison7@gmail.com>
2016-09-12 23:17:27 -0700

    WIP: resurrecting .zshrc

--- zsh/zshrc.sh
+++ zsh/zshrc.sh
@@ -1,22 +1,20 @@
-TMUX_BASE_SESSION_NAME=base
-DIR=~/config/shell
+_cwd=$(pwd)
+cd ~/src/shell-config
 
-source $DIR/lib.sh
-source $DIR/lib_zsh.sh
-source $DIR/path.sh
+. lib.sh
+. lib_zsh.sh
+. path.sh
 
-if [[ -z $TMUX ]] ; then
-    tmux attach -t $TMUX_BASE_SESSION_NAME || tmux new-session -s $TMUX_BASE_SESSION_NAME
-fi
+. git-functions.sh
+. env.sh
+__dan_is_osx && . osx_env.sh
+. dircolors.sh
+. zsh/zsh.sh
+. prompt.sh
+. fasd.sh
+. alias.sh
+. zsh/bindings.sh
+[[ -f extra.sh ]] && . extra.sh
+. tmux.sh
 
-source $DIR/git-functions.sh
-source $DIR/env.sh
-__dan_is_osx && source $DIR/osx_env.sh
-source $DIR/dircolors.sh
-source $DIR/zsh/zsh.sh
-source $DIR/prompt.sh
-source $DIR/fasd.sh
-source $DIR/alias.sh
-source $DIR/zsh/bindings.sh
-[[ -f extra.sh ]] && source $DIR/extra.sh
-source $DIR/tmux.sh
+cd "$_cwd"
