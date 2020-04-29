5a836c230f64978b27b5ec4a85a7c08043812af1
Dan Davison <dandavison7@gmail.com>
2020-04-29 10:33:15 -0400

    Base prompt off oh-my-zsh default

--- zsh/prompt.sh
+++ zsh/prompt.sh
@@ -1,2 +1,11 @@
 typeset -ag precmd_functions;
 precmd_functions=( __prompt_command ${precmd_functions[@]} )
+
+# Based on oh-my-zsh/themes/robbyrussell.zsh-theme
+PROMPT="%(?:%{$fg_bold[green]%}➜ :%{$fg_bold[red]%}➜ )"
+PROMPT+=' %{$fg[cyan]%}%c%{$reset_color%} $(git_prompt_info)'
+
+ZSH_THEME_GIT_PROMPT_PREFIX="%{$fg_bold[blue]%}git:(%{$fg[red]%}"
+ZSH_THEME_GIT_PROMPT_SUFFIX="%{$reset_color%} "
+ZSH_THEME_GIT_PROMPT_DIRTY="%{$fg[blue]%}) %{$fg[yellow]%}✗"
+ZSH_THEME_GIT_PROMPT_CLEAN="%{$fg[blue]%})"
