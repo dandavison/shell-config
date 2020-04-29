684c55ce838ee09c767e7b1adb1cde8af8d3daf8
Dan Davison <dandavison7@gmail.com>
2020-04-29 10:48:39 -0400

    Change prompt: no spaces, no "git:" prefix

--- zsh/prompt.sh
+++ zsh/prompt.sh
@@ -2,10 +2,17 @@ typeset -ag precmd_functions;
 precmd_functions=( __prompt_command ${precmd_functions[@]} )
 
 # Based on oh-my-zsh/themes/robbyrussell.zsh-theme
-PROMPT="%(?:%{$fg_bold[green]%}➜ :%{$fg_bold[red]%}➜ )"
-PROMPT+=' %{$fg[cyan]%}%c%{$reset_color%} $(git_prompt_info)'
+# But using __git_ps1 from https://github.com/git/git/blob/master/contrib/completion/git-prompt.sh
 
-ZSH_THEME_GIT_PROMPT_PREFIX="%{$fg_bold[blue]%}git:(%{$fg[red]%}"
+export GIT_PS1_SHOWDIRTYSTATE=yes
+export GIT_PS1_UNSTAGED="અ "
+export GIT_PS1_STAGED="જ "
+
+
+PROMPT="%(?:%{$fg_bold[green]%}➜ :%{$fg_bold[red]%}➜ )"
+PROMPT+='%{$fg[cyan]%}%c%{$reset_color%}%{$fg[red]%}$(__git_ps1 "(%s)")%{$reset_color%} '
+
+ZSH_THEME_GIT_PROMPT_PREFIX="%{$fg_bold[blue]%}(%{$fg[red]%}"
 ZSH_THEME_GIT_PROMPT_SUFFIX="%{$reset_color%} "
 ZSH_THEME_GIT_PROMPT_DIRTY="%{$fg[blue]%}) %{$fg[yellow]%}✗"
 ZSH_THEME_GIT_PROMPT_CLEAN="%{$fg[blue]%})"
