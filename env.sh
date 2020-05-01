9cf8f1a436062196a80d5cc329d05f352061b914
Dan Davison <dandavison7@gmail.com>
2020-04-30 21:59:17 -0400

    fzf

--- env.sh
+++ env.sh
@@ -13,6 +13,8 @@ export OBJC_DISABLE_INITIALIZE_FORK_SAFETY=YES
 export XENOCANTO_DATA_DIRECTORY=/tmp/xeno-quero-data
 export EXA_COLORS=$(tr '\n' ':' < exa-colors)
 is_zsh && export HISTFILE=~/.eternal_shell_history
+fzf-set-environment-variables
+
 # To add local TeX .sty files:
 #   Add to /usr/local/texlive/texmf-local/tex/latex/local
 #   Run `texhash`
