fd2e21cc37177506ec6afa1701c76d00f921557e
Dan Davison <dandavison7@gmail.com>
2020-05-01 17:48:41 -0400

    Delete unused prompt variables

--- zsh/prompt.sh
+++ zsh/prompt.sh
@@ -11,8 +11,3 @@ export GIT_PS1_STAGED="જ "
 
 PROMPT="%(?:%{$fg_bold[cyan]%}%c:%{$fg[red]%}%c)%{$reset_color%}"
 PROMPT+='%{$fg[red]%}$(__git_ps1 "(%s)")%{$reset_color%} '
-
-ZSH_THEME_GIT_PROMPT_PREFIX="%{$fg_bold[blue]%}(%{$fg[red]%}"
-ZSH_THEME_GIT_PROMPT_SUFFIX="%{$reset_color%} "
-ZSH_THEME_GIT_PROMPT_DIRTY="%{$fg[blue]%}) %{$fg[yellow]%}✗"
-ZSH_THEME_GIT_PROMPT_CLEAN="%{$fg[blue]%})"
