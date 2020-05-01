e8a149268577701af534abe3b3d3ac5b54134552
Dan Davison <dandavison7@gmail.com>
2020-05-01 17:48:28 -0400

    Simplify prompt

--- zsh/prompt.sh
+++ zsh/prompt.sh
@@ -9,8 +9,8 @@ export GIT_PS1_UNSTAGED="અ "
 export GIT_PS1_STAGED="જ "
 
 
-PROMPT="%(?:%{$fg_bold[green]%}➜ :%{$fg_bold[red]%}➜ )"
-PROMPT+='%{$fg[cyan]%}%c%{$reset_color%}%{$fg[red]%}$(__git_ps1 "(%s)")%{$reset_color%} '
+PROMPT="%(?:%{$fg_bold[cyan]%}%c:%{$fg[red]%}%c)%{$reset_color%}"
+PROMPT+='%{$fg[red]%}$(__git_ps1 "(%s)")%{$reset_color%} '
 
 ZSH_THEME_GIT_PROMPT_PREFIX="%{$fg_bold[blue]%}(%{$fg[red]%}"
 ZSH_THEME_GIT_PROMPT_SUFFIX="%{$reset_color%} "
