7e8d7e5eb8fd0733acfbbd02bf4e2baa018f299b
Dan Davison <dandavison7@gmail.com>
2021-04-02 08:34:23 -0400

    insert fzf file path in command line

--- zsh/lib.sh
+++ zsh/lib.sh
@@ -5,3 +5,16 @@ cp-desktop-latest () {
 cp-tmp-latest () {
     cp -r /tmp/*(om[1]) $1
 }
+
+function insert-fzf-path-in-command-line() {
+    # https://github.com/Julian/dotfiles/blob/4da75802dd2d9a7246c8b7dbbaed8a99c315a2ed/.config/zsh/commands.zsh#L44-L54
+    # Copied from https://github.com/garybernhardt/selecta/blob/master/EXAMPLES.md
+    local selected_path
+    echo
+    selected_path=$(fd --type file | fzf) || return
+    LBUFFER+="${(q-)selected_path}"
+    zle reset-prompt
+}
+
+zle -N insert-fzf-path-in-command-line
+bindkey "^F" insert-fzf-path-in-command-line
