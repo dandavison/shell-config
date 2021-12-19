59a4b92fc8e7558ec17578600d72e3cba45a4c7c
Dan Davison <dandavison7@gmail.com>
2021-12-19 11:43:26 -0500

    lib_sourcegraph

--- lib_sourcegraph.sh
+++ lib_sourcegraph.sh
@@ -1,14 +1,26 @@
-sourcegraph-search-files-0() {
-    local root=$(git rev-parse --show-toplevel)
-    src search --json $@ | jq -r ".Results[] | select(.__typename=\"FileMatch\") | \"$root/\(.file.path)\"" | sed -E "s,^$HOME,~,"
+sourcegraph-search-files() {
+    local repo=$(git remote get-url origin | sed 's,^https://,,')
+    src search --stream --json "repo:$repo $@" | jq -r ".path"
 }
 
-sourcegraph-search-files() {
-    src search --stream --json $@ | jq -r ".path"
+sourcegraph-search-files-scala-strato() {
+    sourcegraph-search-files "file:\.(strato|scala)$ $@"
+}
+
+sourcegraph-search-files-scala() {
+    sourcegraph-search-files "file:\.scala$ $@"
 }
 
 sourcegraph-search() {
     git grep -F --function-context $@ -- $(sourcegraph-search-files $@)
+}
+
+sourcegraph-search-scala() {
+    git grep -F --function-context $@ -- $(sourcegraph-search-files-scala $@)
+}
+
+sourcegraph-search-scala-strato() {
+    git grep -F --function-context $@ -- $(sourcegraph-search-files-scala-strato $@)
 }
 
 rgd-scala() {
