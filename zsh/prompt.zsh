611b1a39a3d17e0a9836dc1b4bd9e918f8cfb56c
Dan Davison <dandavison7@gmail.com>
2024-01-02 09:56:24 -0500

    Reformat prompt construction

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -6,8 +6,17 @@ export GIT_PS1_SHOWDIRTYSTATE=yes
 export GIT_PS1_UNSTAGED="અ "
 export GIT_PS1_STAGED="જ "
 
-PROMPT="%(?:%{$fg_bold[cyan]%}%c:%{$fg[red]%}%c)%{$reset_color%}"
-PROMPT+='%{$fg[red]%}$(__git_ps1 "(%s)")%{$reset_color%} '
+PROMPT='%(?:'                # Introduce ternary expression using last exit status as condition
+PROMPT+='%{$fg_bold[cyan]%}' # Cyan for condition-true branch
+PROMPT+='%c'                 # Current project / directory name
+PROMPT+=':'                  # ternary expression separator
+PROMPT+='%{$fg[red]%}'       # Red for condition-false branch
+PROMPT+='%c'                 # Current project / directory name
+PROMPT+=')'                  # End ternary expression
+PROMPT+='%{$reset_color%}'
+PROMPT+='%{$fg[red]%}'
+PROMPT+='$(__git_ps1 "(%s)")'
+PROMPT+='%{$reset_color%} '
 
 preexec() {
     [ -n "$DAN_NO_PREEXEC" ] && return
