997a31a799673746bd96ee59928c1faa854dacf7
Dan Davison <dandavison7@gmail.com>
2024-08-21 16:44:07 -0400

    completions

--- completion.sh
+++ completion.sh
@@ -4,6 +4,7 @@ load-completions () {
     autoload -Uz compinit && compinit -C
     eval "$(delta --generate-completion zsh 2>/dev/null)"
     eval "$(temporal completion zsh 2>/dev/null)"
-    eval "$(wezterm shell-completion --shell zsh)"
+    eval "$(uv generate-shell-completion zsh)"
 }
 load-completions
+
