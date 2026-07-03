ecf8c99506a869238b985f0c421aac32256b1ca7
Dan Davison <dandavison7@gmail.com>
2026-07-03 12:11:15 -0400

    Drop slow completions

--- completion.sh
+++ completion.sh
@@ -1,10 +1,11 @@
 load-completions() {
     autoload -Uz compinit && compinit -C
-    source <(delta --generate-completion zsh 2>/dev/null)
-    source <(temporal completion zsh 2>/dev/null)
-    source <(COMPLETE=zsh wormhole)
+    # tsource <(temporal completion zsh 2>/dev/null) ~100ms
+    tsource <(COMPLETE=zsh wormhole)
+    # source <(should --show-completion zsh)  ~400ms
+    # source <(delta --generate-completion zsh 2>/dev/null)
     # eval "$(uv generate-shell-completion zsh)"
-    source <(_NEOMORPHUS_COMPLETE=zsh_source neomorphus)
-    compdef neo=neomorphus
+    # source <(_NEOMORPHUS_COMPLETE=zsh_source neomorphus)
+    # compdef neo=neomorphus
 }
 load-completions
